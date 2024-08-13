7c9371276409121b3a41d68c642b29efc2f74b7b
redreev8 <redreevs@mail.ru>
2024-08-13 13:23:28 +0500

    calendary: added switching months

--- client/src/components/calendary-day/calendary-day.tsx
+++ client/src/components/calendary-day/calendary-day.tsx
@@ -1,43 +1,12 @@
 import { FC } from 'react'
-import style from './calendary-day.module.scss'
-import classNames from 'classnames'
 import CalendaryLine from '../../UI/calendary-line'
-import List from '../../UI/list'
-import Card from '../../UI/card'
-import Title from '../../UI/title'
-interface CalendaryDayProps {
-    className?: string
+import { CalendaryProps } from '../../UI/calendary/calendary'
+interface CalendaryDayProps extends CalendaryProps {
 }
 
-const CalendaryDay: FC<CalendaryDayProps> = ({ className }) => {
-    const cl = classNames(style.logo, className)
+const CalendaryDay: FC<CalendaryDayProps> = ({ month, year, data }) => {
     return (
-        <CalendaryLine month={8} year={2024} data={{
-            8: {
-                1: <List isBorder>
-                    <Card>
-                        <Title level={3}>Title level 3</Title>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam molestias corporis atque enim animi sapiente voluptas porro. Earum quaerat similique velit rem autem, repudiandae, ut corrupti sed iusto eaque quis.</p>
-                    </Card>
-                    <Card>
-                        <Title level={3}>Title level 3</Title>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam molestias corporis atque enim animi sapiente voluptas porro. Earum quaerat similique velit rem autem, repudiandae, ut corrupti sed iusto eaque quis.</p>
-                    </Card>
-                    <Card>
-                        <Title level={3}>Title level 3</Title>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam molestias corporis atque enim animi sapiente voluptas porro. Earum quaerat similique velit rem autem, repudiandae, ut corrupti sed iusto eaque quis.</p>
-                    </Card>
-                    <Card>
-                        <Title level={3}>Title level 3</Title>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam molestias corporis atque enim animi sapiente voluptas porro. Earum quaerat similique velit rem autem, repudiandae, ut corrupti sed iusto eaque quis.</p>
-                    </Card>
-                    <Card>
-                        <Title level={3}>Title level 3</Title>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam molestias corporis atque enim animi sapiente voluptas porro. Earum quaerat similique velit rem autem, repudiandae, ut corrupti sed iusto eaque quis.</p>
-                    </Card>
-                </List>
-            }
-        }}></CalendaryLine>
+        <CalendaryLine month={month} year={year} data={data}></CalendaryLine>
     )
 }
 
--- client/src/components/calendary-moth/calendary-moth.tsx
+++ client/src/components/calendary-moth/calendary-moth.tsx
@@ -1,30 +1,19 @@
 import { FC } from 'react'
 import style from './calendary-moth.module.scss'
 import Calendary from '../../UI/calendary'
-import Tag from '../../UI/tag'
-import List from '../../UI/list'
 import ScrollShadow from '../../UI/scroll-shadow'
+import { CalendaryProps } from '../../UI/calendary/calendary'
 
-interface CalendaryMothProps {
+interface CalendaryMothProps extends CalendaryProps {
 }
 
-const CalendaryMoth: FC<CalendaryMothProps> = ({ }) => {
+const CalendaryMoth: FC<CalendaryMothProps> = ({ month, year, data}) => {
     return (
 		<ScrollShadow className={ style.scroll }>
 			<Calendary 
-				month={ 6 } 
-				year={ 2024 }  
-				data={{
-					6: {
-						'6': <ScrollShadow><List isRow isBorder><Tag>content</Tag><Tag>content</Tag><Tag>content</Tag><Tag>content</Tag></List></ScrollShadow>,
-						'7': <List><Tag>content</Tag></List>,
-						'8': <List><Tag>content</Tag></List>,
-						'30': <List><Tag>content</Tag></List>,
-					},
-					'5': {
-						'6': <List><Tag>content</Tag></List>
-					} 
-				}}
+				month={ month } 
+				year={ year }  
+				data={ data }
 			>
 			</Calendary>
 		</ScrollShadow>
