ae2180d62b4be42070028d0b3a538f3136151264
redreev8 <redreevs@mail.ru>
2024-08-13 13:49:40 +0500

    calendary: fixed bug with the ruling month

--- client/src/UI/calendary-title/calendary-title.tsx
+++ client/src/UI/calendary-title/calendary-title.tsx
@@ -15,12 +15,13 @@ interface CalendaryTitleProps {
 const CalendaryTitle: FC<CalendaryTitleProps> = ({ month, year, onClick, className }) => {
     const nextMonth = month + 1 === 12 ? 0 : month + 1
     const prevMonth = month - 1 === -1 ? 11 : month - 1
+    
     return (
         <div className={ classNames(style['calendary-title'], className) }>
-            <Title>{ nameMonth[month] } {year}</Title>
+            <Title level={2} className={ style['calendary-title__moth'] }>{ nameMonth[month] } {year}</Title>
 			<List isBorder isRow>
-				<Btn onClick={ () => onClick(prevMonth) }>{ nameMonth[prevMonth] }</Btn>
-				<Btn onClick={ () => onClick(nextMonth) }>{ nameMonth[nextMonth] }</Btn>
+				<Btn className={ style['calendary-title__btn'] } onClick={ () => onClick(prevMonth) }>{ nameMonth[prevMonth] }</Btn>
+				<Btn className={ style['calendary-title__btn'] } onClick={ () => onClick(nextMonth) }>{ nameMonth[nextMonth] }</Btn>
 			</List>
         </div>
     )
