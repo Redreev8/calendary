fcecfa170614bcbb171a1b666c0d91bf62eb1c46
redreev8 <redreevs@mail.ru>
2024-09-06 16:24:10 +0500

    UI: container added children setting padding and border

--- client/src/UI/container/container.tsx
+++ client/src/UI/container/container.tsx
@@ -6,21 +6,25 @@ interface ContainerProps {
     children: ReactNode
     className?: string
     width?: string
+    pg?: ('xl' | 'lg' | 'bg' | 'g' | 'md' | 'sm' | 'xs' | 'tiny')[]
 }
 
-const Container: FC<ContainerProps> = ({ children, width='default', className }) => {
+const Container: FC<ContainerProps> = ({ children, width='default', pg=[], className }) => {
     const cl = classNames(
         style.container, 
         className, 
         style[`container--${width}`],
         Array.isArray(children) && style[`container--${children.length}`],
-        {}
     )
+    console.log(style);
+    
     return (
         <div className={ cl }>
             {
-                Children.map(children, child => (
-                    <div className={ style['container__col'] }>{ child }</div>
+                Children.map(children, (child, i) => (
+                    <div 
+                        className={ classNames(style['container__col'], style[`container__col--p-${pg[i] ?? 'g'}`],) }
+                    >{ child }</div>
                 ))
             }
         </div>
